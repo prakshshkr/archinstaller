8d21b5f05ab0a5aa022d1b9e8da1b786bafa57e6
prakshshkr <63063596+prakshshkr@users.noreply.github.com>
2020-09-11 15:23:21 -0400

    Update archinstaller.sh

--- archinstaller.sh
+++ archinstaller.sh
@@ -1,6 +1,6 @@
 #parted -s -a optimal /dev/sdb mklabel gpt -- mkpart primary fat32 2048 512M -- set 1 esp on 
 pacman -Syy
-pacman -S reflector
+pacman -S --noconfirm reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
 mount /dev/sdb2 /mnt
