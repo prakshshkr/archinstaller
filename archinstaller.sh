084d127b9f9d0be05e9797e12aef2fa96c644ab1
prakshshkr <63063596+prakshshkr@users.noreply.github.com>
2020-09-26 21:35:06 +0000

    Update archinstaller.sh
    add user input

--- archinstaller.sh
+++ archinstaller.sh
@@ -1,9 +1,11 @@
 #parted -s -a optimal /dev/sdb mklabel gpt -- mkpart primary fat32 2048 512M -- set 1 esp on 
+echo which disk
+read diskname
 pacman -Syy
 pacman -S --noconfirm reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
-mount /dev/sdb2 /mnt
+mount /dev/$diskname /mnt
 pacstrap /mnt base linux linux-firmware vim nano
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
