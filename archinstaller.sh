faa0c9c283b330b8bef5b186850ee6d68bd9afb3
prakshshkr <63063596+prakshshkr@users.noreply.github.com>
2020-10-04 15:53:33 -0400

    Update archinstaller.sh
    specify drive

--- archinstaller.sh
+++ archinstaller.sh
@@ -1,11 +1,10 @@
-#parted -s -a optimal /dev/sdb mklabel gpt -- mkpart primary fat32 2048 512M -- set 1 esp on 
-echo which disk
-read diskname
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
 pacman -Syy
 pacman -S --noconfirm reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
-mount /dev/$diskname /mnt
+mount /dev/sdb2 /mnt
 pacstrap /mnt base linux linux-firmware vim nano
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
